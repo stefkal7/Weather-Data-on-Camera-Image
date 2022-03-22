7e6b08a46946e84675a74e3c4eaa1dd0a6ac87c4
stefkal7 <75183178+stefkal7@users.noreply.github.com>
2022-03-22 23:26:17 +0200

    Add files via upload

---
+++ meteodata1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import sys
+import os
+import subprocess
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+import dropbox
+import ftplib
+from ftplib import FTP
+
+meteo = open('/home/weewx/realtime.txt').read()
+data = meteo.split()
+print(data[0],'|',data[1],'|','Baro:', data[10],'hPa'' |',' Temp:',data[2],'C' ' |',' Hum:', data[3],'%' ' |','Rain:',data[9],'mm''|','Rain Rate:',data[8],'mm/h''|','Wind:',data[5],'km/h','at' ,data[11])
+with open('/home/pi/meteo.txt', 'w') as f:   
+    print(data[0],'|',data[1],'|','Baro:', data[10],'hPa'' |','Temp:',data[2],'C' ' |','Hum:', data[3],'%' '|','Rain:',data[9],'mm''|','Rain Rate:',data[8],'mm/h''|','Wind:',data[5],'km/h','at' ,data[11], file=f)  # Python 3.x
+    
+subprocess.call('ffmpeg -rtsp_transport tcp -i rtsp://admin:freeze07@192.168.1.8:34567/user=admin_password=freeze07_channel=1_stream=0.sdp -q:v 1 -y -v quiet /home/pi/grab.jpg', shell=True)
+
+#"Draw a text on an Image, saves it"
+text=open('/home/pi/meteo.txt').read()
+image=Image.open('/home/pi/grab.jpg')
+font = ImageFont.truetype("/usr/share/fonts/gentium-basic/GenBkBasR.ttf",34)
+draw=ImageDraw.Draw(image)
+#draw backround text
+draw.rectangle((1920,0 ,0,45), fill='#09189c')
+# draw text
+draw.text((10,2), text=text, font=font, fill=(255,255,255, 255))
+# save file
+image.save('/home/pi/image.jpg')
+#os.chmod('/home/pi/image.jpg', 777)
+#send file to Dropbox
+app_key = 'ijwrmofe2ic9iea'
+app_secret = '4g34bunmvgnx7ed'
+access_token = 'flYfUTH48TwAAAAAAAAAAXcNdvPHzd7-s-ZauJ6M6nONe7r98d1-kZLrS3p4olek'
+file_from = '/home/pi/image.jpg'  
+file_to = '/Webcam Roofeye/image.jpg'      
+def upload_file(file_from, file_to):
+    dbx = dropbox.Dropbox(access_token)
+    f = open(file_from, 'rb')
+    dbx.files_upload(f.read(), file_to, mode=dropbox.files.WriteMode("overwrite") )
+upload_file(file_from,file_to)
+#send image ftp to Northmeteo
+filename = "image.jpg"
+ftp = FTP("northmeteo.gr")
+ftp.login("nick2k4", "?Hpi8y08")
+ftp.cwd("/httpdocs/cam/tagarades")
+myfile = open('/home/pi/grab.jpg', 'rb') 
+ftp.storbinary('STOR '+ filename, myfile)
+ftp.quit()
+
