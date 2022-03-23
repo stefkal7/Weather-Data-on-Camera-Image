d3a46e7148a10d2ac2ca9be787144d3ca27c2d51
stefkal7 <75183178+stefkal7@users.noreply.github.com>
2022-03-23 10:25:04 +0200

    Update meteodata1.py

--- meteodata1.py
+++ meteodata1.py
@@ -13,7 +13,7 @@ print(data[0],'|',data[1],'|','Baro:', data[10],'hPa'' |',' Temp:',data[2],'C' '
 with open('/home/pi/meteo.txt', 'w') as f:   
     print(data[0],'|',data[1],'|','Baro:', data[10],'hPa'' |','Temp:',data[2],'C' ' |','Hum:', data[3],'%' '|','Rain:',data[9],'mm''|','Rain Rate:',data[8],'mm/h''|','Wind:',data[5],'km/h','at' ,data[11], file=f)  # Python 3.x
     
-subprocess.call('ffmpeg -rtsp_transport tcp -i rtsp://admin:freeze07@192.168.1.8:34567/user=admin_password=freeze07_channel=1_stream=0.sdp -q:v 1 -y -v quiet /home/pi/grab.jpg', shell=True)
+subprocess.call('ffmpeg -rtsp_transport tcp -i rtsp://admin: password@192.168.1.8:34567/user=admin_password=password_channel=1_stream=0.sdp -q:v 1 -y -v quiet /home/pi/grab.jpg', shell=True)
 
 #"Draw a text on an Image, saves it"
 text=open('/home/pi/meteo.txt').read()
@@ -26,7 +26,6 @@ draw.rectangle((1920,0 ,0,45), fill='#09189c')
 draw.text((10,2), text=text, font=font, fill=(255,255,255, 255))
 # save file
 image.save('/home/pi/image.jpg')
-#os.chmod('/home/pi/image.jpg', 777)
 #send file to Dropbox
 app_key = 'your apikey'
 app_secret = 'your secret'
@@ -38,7 +37,7 @@ def upload_file(file_from, file_to):
     f = open(file_from, 'rb')
     dbx.files_upload(f.read(), file_to, mode=dropbox.files.WriteMode("overwrite") )
 upload_file(file_from,file_to)
-#send image ftp to Northmeteo
+#send image ftp 
 filename = "image.jpg"
 ftp = FTP("your_ftp.com")
 ftp.login("user", "password")
